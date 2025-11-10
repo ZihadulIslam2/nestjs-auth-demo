ce0e62f11c05520675a615e5c42629cbeb5971f6
zihadulislam2 <zihadul708@gmail.com>
2025-11-11 00:29:13 +0600

    update auth.module.ts and main.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserService } from 'src/user/user.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'default_secret_key',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   providers: [AuthService, UserService],
 })
 export class AuthModule {}
